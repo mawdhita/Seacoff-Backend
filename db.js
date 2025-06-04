c2846ac8627d7cfb56a663845fbb60d9ad18c9dd
mawar <mawar@gmail.com>
2025-06-04 12:28:59 -0700

    baru

--- db.js
+++ db.js
@@ -6,9 +6,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  waitForConnections: true,
+   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  connectTimeout: 10000 // 10 detik
 });
 
 // Opsional: tes koneksi awal
