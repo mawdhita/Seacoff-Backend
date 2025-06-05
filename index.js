75734c75ef62e71a785efa0cbf0c0266713da75f
mawar <mawar@gmail.com>
2025-06-05 00:33:14 -0700

    last dan

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// server.js atau app.js
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.use('/uploads', express.static('uploads')); 
 
 // Hardcoded BASE_URL (tanpa .env)
 const BASE_URL = 'https://seacoff-backend.vercel.app';
