29b1717b51fea486bd20567ef218b2f45509a9b5
mawar <mawar@gmail.com>
2025-06-05 06:10:18 -0700

    berhasil hore

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db'); // tambahkan ini!
-const cartController = require('../controllers/cartController');
 
 
 router.post('/cart', async (req, res) => {
