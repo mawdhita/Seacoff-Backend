a614679e219a7e668b57a0c76553f29863a6913d
mawar <mawar@gmail.com>
2025-06-15 10:06:48 +0700

    jangan eror

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://seacoff-frontend.vercel.app',
+    'http://localhost:3000',  // untuk development
+    'http://localhost:3001'   // jika menggunakan port lain
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
